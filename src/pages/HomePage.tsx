28b6cfb76e61f67c22ee1d6ad4d7b73fa7283043
Gustavo Marques <gustavomarques.gml@gmail.com>
2022-06-22 16:44:50 -0300

    Creation of home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,20 @@
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
-    return <h2>Home</h2>;
-  }
-  
+  return (
+    <div>
+      <div className="title-container">
+        <h1 className="form-title"> Gamma Judge Admin </h1>
+      </div>
+      <div className="form-container">
+        <Link className="input-container" to="/problems">
+          <Button>Problems</Button>
+        </Link>
+        <Link className="input-container" to="/contests">
+          <Button>Contests</Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
